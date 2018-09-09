6040f68b54ebaf736131dc7618e45e599ede825c
Noah-Amar <ne.amar@outlook.com>
2018-09-08 20:57:02 -0500

    pull request

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+server.listen(5000, () => console.log('API running on port 5000'));
+

10e0a93a8d13b64e32320308b6f50ce5be07931a
Noah-Amar <ne.amar@outlook.com>
2018-09-21 17:50:10 -0500

    added dependencies

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send("Hello World");
 });
 
-server.listen(5000, () => console.log('API running on port 5000'));
+
+const port = process.env.PORT || 5000;
+server.listen(5000, () => console.log(`API running on port ${port}`));
 
